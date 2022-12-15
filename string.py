e5e785603dabe851568ec543c960a76ba6ffeb2b
SINCHANA BS <sinchanabs91@gmail.com>
2022-12-15 19:26:36 +0530

    init

--- string.py
+++ string.py
@@ -4,7 +4,6 @@ apple ='She said, \"She wants to eat an apple" today'
 print("Hello," + friend)
 print(apple)
 print(apple[3])
-
 print("lets use a for loop")
 for charcter in apple:
     print(charcter)
